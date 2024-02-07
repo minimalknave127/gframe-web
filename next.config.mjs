03699d8ff4a8734111eaf244aa566dd3c228f299
minimalknave127 <tadeas.simandl@gmail.com>
2024-02-07 13:49:15 +0100

    working

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/dashboard",
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
